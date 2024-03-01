59e38b9c668e357401b3fdf2d9942976e195da89
091189Ashish <rajposhanupgrade@gmail.com>
2024-03-01 15:59:16 +0530

    third commit

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-alert('Hello Ashish Update');
-alert('other code');
-=======
-alert('Hello Ashish Sharma');
->>>>>>> 0b3fe300c4bb1a59b880dcb9b18355566cb34b05
-alert ('change again');
+alert('remove changes');
